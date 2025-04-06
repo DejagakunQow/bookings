009c9c09f6c714134c6378b62dad8accbd07624b
Jamac Ali <jamaqow86@gmail.com>
2025-04-06 15:37:19 +0300

    add reservation and availability pages, update routes and styles

--- cmd/Web/routes.go
+++ cmd/Web/routes.go
@@ -3,12 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/DejagakunQow/bookings/pkg/handlers"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/dejagakun/bookings-app/pkg/config"
+	"github.com/dejagakun/bookings-app/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
-func routes() http.Handler {
+func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
@@ -17,10 +18,15 @@ func routes() http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
-
 }
