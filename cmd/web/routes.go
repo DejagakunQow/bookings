459f8b50eb4d6ee5bf6beccfc222bcae19814e3e
Jamac Ali <jamaqow86@gmail.com>
2026-01-16 14:41:17 +0300

    Temporarily disable middleware to render homepage

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,18 +6,12 @@ import (
 	"github.com/DejagakunQow/bookings/cmd/web/internal/config"
 	"github.com/DejagakunQow/bookings/cmd/web/internal/handlers"
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
-	// Middleware
-	mux.Use(middleware.Recoverer)
-	mux.Use(NoSurf)
-	mux.Use(SessionLoad(app))
-
-	// Application routes
+	// TEMP: no middleware (sessions / CSRF disabled for now)
 	mux.Get("/", handlers.Repo.Home)
 
 	// Static files
